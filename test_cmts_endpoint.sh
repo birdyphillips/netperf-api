ea98dfed723285e3ceaeec6a347b8a44f46c8697
aphillips <aphillips@example.com>
2026-02-27 18:43:02 +0000

    Remove test script

--- test_cmts_endpoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Test CMTS Modem Info API Endpoint
-
-echo "Testing vCMTS endpoint..."
-curl "http://localhost:5000/api/cmts/modem/info?cmts_host=apc01k1dccc&cm_mac=e0db.d161.3d18&cmts_type=vcmts"
-
-echo -e "\n\nTesting iCMTS endpoint..."
-curl "http://localhost:5000/api/cmts/modem/info?cmts_host=cts01k1dccc&cm_mac=0cb9.379c.64b4&cmts_type=icmts"
